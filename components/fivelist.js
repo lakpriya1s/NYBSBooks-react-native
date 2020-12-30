7118429363f46c5c09ed7d179873868449da5457
Lakpriya <lakpriyasenevirathna@gmail.com>
2020-12-30 12:18:46 +0530

    top5complate

---
+++ components/fivelist.js
@@ -0,0 +1,79 @@
+import { ActivityIndicator, StyleSheet, FlatList, Text, View, SafeAreaView, Image, TouchableOpacity } from "react-native";
+import React, { Component } from 'react';
+import { Card, Button, Icon } from "react-native-elements";
+import axios from "axios";
+
+class FiveList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    render() { 
+            const renderItem = ({ item }) => (
+                <TouchableOpacity onPress={() => {
+                    this.props.nav.navigate("Book", {name: item.title, book: item})
+                  }}>
+                    <Card containerStyle={{}} wrapperStyle={{}}>
+                    <Card.Title>{item.title}</Card.Title>
+                        <View
+                            style={{
+                            position: "relative",
+                            alignItems: "center"
+                            }}>
+                            <Image
+                            style={styles.image}
+                            resizeMode="contain"
+                            source={{
+                                uri: item.book_image
+                            }}
+                            PlaceholderContent={<ActivityIndicator />}>
+                            </Image>              
+                        </View>
+                    </Card>
+                </TouchableOpacity>
+            );
+          
+            return (
+                <SafeAreaView style={styles.container}>
+                  <FlatList
+                    data={this.props.books.books}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.primary_isbn10}
+                  />
+                </SafeAreaView>
+            );
+        }
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 0,
+    },
+    image: {
+        flex: 0,
+        width:"100%",
+        height: 500,
+        position: "relative"
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    horizontal: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10
+    },
+    item: {
+        backgroundColor: '#63ccff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 20
+    },
+})
+ 
+export default FiveList;
--- pages/top5.js
+++ pages/top5.js
@@ -1,17 +1,95 @@
-import { View } from "react-native";
+import { ActivityIndicator, StyleSheet, FlatList, Text, View, SafeAreaView, Image, TouchableOpacity, FlatLists } from "react-native";
+import React, { Component } from 'react';
+import { Card, Button, Icon } from "react-native-elements";
+import axios from "axios";
+import FiveList from "../components/fivelist";
 
 class TopBooks extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            data: [],
+            isLoaded: false,
+        }
     }
+
+    componentDidMount(){
+        axios.get('https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=OPZ9CT7rKrV6lGmEJxs9bAymoBiyAIsI')
+        .then(response => {
+            this.setState({ 
+            isLoaded: true,
+            data: response.data.results.lists
+          });
+          console.log(this.state.data);
+        })
+        .catch((err)=> {
+            console.log(err);
+        })  
+    }
+    
     render() { 
-        return ( 
-            <View>
-                <Text>Hi</Text>
-            </View>
-         );
+        var {isLoaded, data} = this.state;
+
+        if(!isLoaded){
+            return <View style={[styles.container, styles.horizontal]}>
+            <ActivityIndicator size="large" />
+          </View>
+        }else{
+            const renderList = ({ item }) => (
+                <View>
+                    <TouchableOpacity onPress={() => {
+                        this.props.navigation.navigate("Category", {titleid: item.list_name_encoded, name: item.display_name})
+                    }}>
+                    <Card>
+                        <Card.Title>{item.display_name}</Card.Title>
+                    </Card>
+                  </TouchableOpacity>
+                    
+                    <FiveList books={item} nav={this.props.navigation}/>
+                </View>
+            );
+
+            return (
+                <SafeAreaView style={styles.container}>
+                  <FlatList
+                    data={data}
+                    renderItem={renderList}
+                    keyExtractor={item => item.list_id}
+                  />
+                </SafeAreaView>
+              );
+        }
     }
 }
- 
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 0,
+    },
+    image: {
+        flex: 0,
+        width:"100%",
+        height: 500,
+        position: "relative"
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    horizontal: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10
+    },
+    item: {
+        backgroundColor: '#63ccff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 20
+    },
+})
+
 export default TopBooks;
